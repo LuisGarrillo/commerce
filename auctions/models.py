884022f95f0980669dbbef5ec870da4811fbea04
Luis Garrillo <lggarrillo@urbe.edu.ve>
2024-04-07 12:47:51 -0400

    added auction model

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
     pass
+
+class Auction(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=500)
+    cover = models.ImageField()
+    initial_bid = models.DecimalField(decimal_places=2, max_digits=11)
+    status = models.IntegerField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="won_auctions", null=True, blank=True)
